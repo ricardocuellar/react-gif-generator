3a2ee1c845ba1f7448ec3e12df2c09dd35b5a3ec
ricardocuellar <rcuellarsn@gmail.com>
2024-10-16 11:50:27 -0600

    Init GifApp

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,23 @@
+//Después del aversión 17 ya no es necesario importarlo por todos lados. 
+//import React from 'react'
+
+import { useState } from "react";
+
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState([ 'One Puch' ]); //Vacio genera undefined
+
+    console.log(categories);
+  return (
+    <>
+        {/* Titulo */}
+        <h1>GifExpertApp</h1>
+    
+        {/* Input */}
+    
+        {/* Listado de Items Gifs */}
+        
+            {/* GifItem */}
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
+import './styles.css'; //En el componente padre se aplica de forma global
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GifExpertApp />
+  </StrictMode>,
+);
